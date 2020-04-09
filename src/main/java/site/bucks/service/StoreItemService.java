9476d1b48e84ac444396b0b0d9be43c4eee2bae0
Administrator <Administrator@COMJK-906071434>
2020-04-09 23:54:23 +0900

    test

--- src/main/java/site/bucks/service/StoreItemService.java
+++ src/main/java/site/bucks/service/StoreItemService.java
@@ -3,6 +3,7 @@ package site.bucks.service;
 import site.bucks.dto.Sale;
 
 public interface StoreItemService {
-	void saleRecord(Sale sale);
+	void saleRecord1(Sale sale);
+	void saleRecord2(Sale sale);
 
 }
--- src/main/java/site/bucks/service/StoreItemServiceImpl.java
+++ src/main/java/site/bucks/service/StoreItemServiceImpl.java
@@ -1,5 +1,6 @@
 package site.bucks.service;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,31 +17,35 @@ public class StoreItemServiceImpl implements StoreItemService{
 	@Autowired
 	private StoreItemDAO storeItemDAO;
 	
+//	@Autowired
+//	HashMap<String, String> map;
+	
+//	재료로 받을때 조합
 	@Override
-	public void saleRecord(Sale sale) {
+	public void saleRecord1(Sale sale) {
 		
 		storeItemDAO.insertSale(sale);
 		
 		ProductRecipe pr=storeItemDAO.selectProduct(sale);
+		
 		String item1=pr.getItem1();
 		String item2=pr.getItem2();
 		String item3=pr.getItem3();
 		
 		int qty = sale.getSaleQty();
 		
-		calculator(qty,item1);
+		calculator1(qty,item1);
 		
 		if(item2!=null) {
-			calculator(qty, item2);
+			calculator1(qty, item2);
 		}
 		if(item3!=null) {
-			calculator(qty, item3);
+			calculator1(qty, item3);
 		}
 		
 	}
-	
-	
-	public void calculator(int qty, String item) {
+
+	public void calculator1(int qty, String item) {
 //		여기서 추가합 맵은 updateRecord 및 updateItem 두개의 메소드에 사용
 		Map<String, Object> map=new HashMap<String, Object>();
 		map.put("item", item);
@@ -62,6 +67,31 @@ public class StoreItemServiceImpl implements StoreItemService{
 		map.put("itemRest",itemRest);
 		
 		storeItemDAO.updateRecord(map);
+	}
+	
+	
+	
+	
+//	재료가 아닌 완제품일때
+	@Override
+	public void saleRecord2(Sale sale) {
+		
+		storeItemDAO.insertSale(sale);
+		
+		int qty = sale.getSaleQty();
+		String item =sale.getSaleProduct();
+		
+		calculator2(qty,item);
+	}
+	
+	public void calculator2(int qty,String item) {
+//		여기서 추가합 맵은 updateRecord 및 updateItem 두개의 메소드에 사용
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("item", item);
+		int itemRest= storeItemDAO.selectItemRest(item);
+		
+		
+//		store 재고 업데이트 메소드
 	}
 	
 	
