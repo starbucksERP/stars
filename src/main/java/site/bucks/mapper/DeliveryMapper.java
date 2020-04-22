bafdcbc92a28fe52bc51eae51ac8db13bd29bc9b
akaRaphael <63382028+akaRaphael@users.noreply.github.com>
2020-04-22 09:58:15 +0900

    ?????

--- src/main/java/site/bucks/mapper/DeliveryMapper.java
+++ src/main/java/site/bucks/mapper/DeliveryMapper.java
@@ -9,7 +9,7 @@ public interface DeliveryMapper {
 	int updateDeliveryReceiptProcess(OrderItem orderItem);
 	
 	
-	int insertDeliveryRequest(Delivery delivery); // 근형 
+	int insertDeliveryRequest(Delivery delivery); // 근형  
 	List<Delivery> selectDeliveryList(Delivery delivery); // 근형
 	//int updateDeliveryState(int deliverySeq); // 근형
 	int updateDeliveryReq(int deliverySeq);
