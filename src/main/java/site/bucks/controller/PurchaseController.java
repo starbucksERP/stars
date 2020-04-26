a16f4abd48c3ab66f80a927060f63505e7b384f0
akaRaphael <63382028+akaRaphael@users.noreply.github.com>
2020-04-27 04:35:19 +0900

    ㄹㅋㅋㅁ

--- src/main/java/site/bucks/controller/PurchaseController.java
+++ src/main/java/site/bucks/controller/PurchaseController.java
@@ -1,13 +1,9 @@
 package site.bucks.controller;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
